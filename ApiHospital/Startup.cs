e483bbcc209269e93d577a8f633d8943a2b19fd2
Serra <pacoserranox@gmail.com>
2021-03-01 14:02:54 +0100

    commit

--- ApiHospital/Startup.cs
+++ ApiHospital/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ApiHospital
 {
@@ -30,17 +31,33 @@ namespace ApiHospital
             services.AddTransient<RepositoryHospital>();
             services.AddDbContext<HospitalContext>(options =>
             options.UseSqlServer(cadena));
+
+            services.AddSwaggerGen(
+                options =>
+                {
+                    options.SwaggerDoc(name: "v1", new OpenApiInfo
+                    {
+                        Title = "Api Hospitales",
+                         Version = "v1", Description = "Api Hospitales Core 2021"
+                    });
+                });
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                options =>
+                {
+                    options.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json", name: "Api v1");
+                    options.RoutePrefix = "";
+                });
             app.UseHttpsRedirection();
 
             app.UseRouting();
